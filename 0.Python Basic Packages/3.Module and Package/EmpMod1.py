cdbbf27c97e2dc12920fe592a5bc53ac055c780f
Sachin Gupta <Sachin.Gupta@BANCTECINDIA.IN>
2020-08-02 19:51:23 +0530

    ML

---
+++ 0.Python Basic Packages/3.Module and Package/EmpMod1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  5 14:26:52 2020
+
+@author: Ankush
+"""
+
+#Module1
+
+Eloc = "Pune"
+print("Location : ", Eloc)
+
+
+import EmpMod2
+
+print('After importing Module2')
